adb4f255c27abf95426abd84af2fe54ee03ed557
Tae Lee <taekyu.lee3@gmail.com>
2023-06-23 01:20:48 +1000

    products.js dummy data

--- src/products.js
+++ src/products.js
@@ -1,8 +1,61 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+
+
 export const PRODUCTS = [
-    {
-        id: 1,
-        productName: "IPhone",
-        price: 1349.0,
-        productImage: 
-    }
-]
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 1199.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 2999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Canon Camera",
+    price: 799.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "AMD Ryzen 7 5700X",
+    price: 529.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Samsung Portable SSD",
+    price: 199.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Sony USB 4GB",
+    price: 29.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "MSI RTX 4070TI",
+    price: 1349.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Ipad Pro",
+    price: 899.0,
+    productImage: product8,
+  },
+];
