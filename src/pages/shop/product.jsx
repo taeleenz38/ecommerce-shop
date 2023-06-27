b31fd0effa152d0bf33943c97d5e3fec5708a4ed
Tae Lee <taekyu.lee3@gmail.com>
2023-06-27 18:18:37 +1000

    usestate + context

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ShopContext from "../../context/shop-context"
 
 const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemAmount = cartItems[id]
   return (
     <div className='product'>
       <img src={productImage} />
@@ -9,7 +12,7 @@ const Product = (props) => {
         <p> <b>{productName} </b></p>
         <p>${price}</p>
       </div>
-      <button className='addToCartBtn'>Add To Cart</button>
+      <button className='addToCartBtn' onClick={() => addToCart(id)}>Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}</button>
     </div>
   )
 }
