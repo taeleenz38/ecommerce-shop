cf105fc36d8ea84a9c07af31336a49df2b9a0dcb
Tae Lee <taekyu.lee3@gmail.com>
2023-06-28 22:28:46 +1000

    cart-page

---
+++ src/pages/cart/cart-item.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CartItem = (props) => {
+    const { id, productName, price, productImage } = props.data;
+    return (
+        <div className='cartItem'>
+            <img src={productImage} />
+            <div className='description'>
+                <p><b> {productName}</b></p>
+                <p>${price}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CartItem
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context"
+import CartItem from './cart-item';
+
 
 function Cart() {
+  const { cartItems } = useContext(ShopContext);
   return (
-    <div>
-      Cart
+    <div className='cart'>
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />
+          }
+        })}
+
+      </div>
     </div>
   )
 }
